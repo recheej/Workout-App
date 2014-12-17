2fc974759de575a261acca0bdf6b864227ee75d4
Rechee Jozil <recheejozil@hotmail.com>
2014-12-16 20:06:01 -0500

    First commit

---
+++ Workout App/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Workout App
+//
+//  Created by Rechee Jozil on 12/16/14.
+//  Copyright (c) 2014 Rechee Jozil. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Workout App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Workout App
+//
+//  Created by Rechee Jozil on 12/16/14.
+//  Copyright (c) 2014 Rechee Jozil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
